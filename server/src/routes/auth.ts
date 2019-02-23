e949e7a52b03f82aa59315b3b9922153e5205363
Mikael Couzic <mikaelcouzic@gmail.com>
2019-02-23 11:43:52 +0100

    feat(linkedin): fetch access token

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -9,18 +9,18 @@ const redirectUri = 'https://adapteach-web.herokuapp.com/auth/linkedin/callback'
 export const authRoute: Route = router => {
   router.get('/auth/linkedin/callback', async ctx => {
     const { code } = ctx.query
-    // const url =
-    //   'https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code' +
-    //   '&client_id=' +
-    //   clientId +
-    //   '&client_secret=' +
-    //   clientSecret +
-    //   '&redirect_uri=' +
-    //   redirectUri +
-    //   '&code=' +
-    //   code
-    // const response = await axios.get(url)
+    const url =
+      'https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code' +
+      '&client_id=' +
+      clientId +
+      '&client_secret=' +
+      clientSecret +
+      '&redirect_uri=' +
+      redirectUri +
+      '&code=' +
+      code
+    const response = await axios.get(url)
     ctx.status = 200
-    ctx.body = 'OK'
+    ctx.body = response
   })
 }
